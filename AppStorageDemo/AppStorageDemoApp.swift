b15bd12106d00ba2c1890090b795c578c8d57c4c
Jeremiah <jjessel84@gmail.com>
2020-09-09 10:43:45 -0700

    Initial Commit

---
+++ AppStorageDemo/AppStorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageDemoApp.swift
+//  AppStorageDemo
+//
+//  Created by Jeremiah Jessel on 9/9/20.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
